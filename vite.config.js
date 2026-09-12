2e1d2abbc2008926e8d81e1720f9b8ae1ffcf0da
Himanshurchaudhary <rchaudhary9771@gmail.com>
2026-09-13 02:18:45 +0530

    Initial commit 12-09-20261111223

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   base: '/',
   plugins: [
     tailwindcss(),
+    react(),
+    sitemap({
+      hostname: 'https://theorchardengine.com',  // ← apna domain yahan lagao
+      dynamicRoutes: [
+        '/',
+        '/user/product',
+        '/user/blog',
+        '/user/contect',
+        '/user/about',
+        '/user/termcondition',
+        '/user/privacy',
+        '/user/deleteaccount',
+        '/forgot-password',
+        '/user/categories',
+      ]
+    })
   ],
 })
